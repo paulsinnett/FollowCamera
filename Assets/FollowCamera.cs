0c43c9dd42901e19bbfa59a7dc2ca48591279f0c
Paul Sinnett <paul.sinnett@gmail.com>
2017-04-26 10:09:22 +0100

    demonstrate the problem

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+	public Transform target;
+	public float distance = 10.0f;
+	public float height = 5.0f;
+	public float smooth = 1.0f;
+	Vector3 velocity = Vector3.zero;
+
+	void LateUpdate () {
+
+		Vector3 targetPosition = target.position - Vector3.forward * distance + Vector3.up * height;
+		transform.position = Vector3.SmoothDamp (transform.position, targetPosition, ref velocity, smooth);
+		transform.LookAt (target);
+	}
+}
---
+++ Assets/Move.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	public Rigidbody physics;
+	public float force = 1.0f;
+
+	void Start () {
+
+		physics.AddForce (transform.right * force, ForceMode.Impulse);
+	}
+}
